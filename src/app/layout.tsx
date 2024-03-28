951b5aab4e748dcad0a0367d5abe59f0b11cb37e
viridius-hub <smiledie-hub@yandex.ru>
2024-03-28 20:38:12 +0500

    add metric

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ import '@styles/datepicker.scss'
 import '@styles/slick-theme.scss'
 import '@styles/slick.scss'
 import '@styles/global.scss'
+import Script from "next/script";
 
 export const metadata: Metadata = {
 	title: 'Создание аудиокниг. Озвучивание текстов. AIBooks',
@@ -26,6 +27,7 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode
 }) {
+
 
 	return (
 			<html lang="ru">
@@ -39,6 +41,20 @@ export default function RootLayout({
 					</div>
 					<Footer/>
 				</ContextLayout>
+				<Script id="metrika-counter" strategy="afterInteractive">
+					{`(function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+						m[i].l=1*new Date();
+						for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+						k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+						(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+			
+						ym(92974650, "init", {
+						clickmap:true,
+						trackLinks:true,
+						accurateTrackBounce:true,
+						webvisor:true
+					});`}
+				</Script>
 				</body>
 			</html>
 	)
