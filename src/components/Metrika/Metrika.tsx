f57bda56d81e42e400fe5b54b438d62f29cc03b7
viridius-hub <smiledie-hub@yandex.ru>
2024-04-03 09:13:23 +0500

    fix ya metric

--- src/components/Metrika/Metrika.tsx
+++ src/components/Metrika/Metrika.tsx
@@ -2,37 +2,16 @@
 
 import { useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
-import Script from "next/script";
+import {YMInitializer} from "react-yandex-metrika";
+import ym from 'react-yandex-metrika';
 
 export function Metrika() {
     const pathName = usePathname();
     const searchParams = useSearchParams();
     useEffect(() => {
-        (function () {
-            const PM_YM_COUNTER = 92974650;
-            //@ts-ignore
-            if (typeof window.ym != 'undefined') {
-                //@ts-ignore
-                ym(PM_YM_COUNTER, 'hit', window.location.href);
-            } else {
-                console.log('Метрика не инициализирована');
-            }
-        })();
+        ym('hit', window.location.href);
     }, [pathName, searchParams]);
     return (
-        <Script id="yandex-metrika">
-            {`(function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-						m[i].l=1*new Date();
-						for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-						k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-						(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-			
-						ym(92974650, "init", {
-						clickmap:true,
-						trackLinks:true,
-						accurateTrackBounce:true,
-						webvisor:true
-					});`}
-        </Script>
+        <YMInitializer accounts={[92974650]} options={{webvisor: true, defer: true}} />
     );
 }
